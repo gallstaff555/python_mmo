c6fb2f5442bf5e3ffd3c2af3db5ed37900923ddf
David <davidsolo555@hotmail.com>
2023-10-26 22:46:50 -0700

    Create class for managing world

--- world/tiled_map.py
+++ world/tiled_map.py
@@ -1,5 +1,9 @@
 import pytmx 
 
+from config.config import Config
+
+cfg = Config()
+
 class TiledMap:
     def __init__(self, filename):
         self.tmx_data = pytmx.load_pygame(filename)
@@ -15,9 +19,7 @@ class TiledMap:
                 for row in range(self.tmx_data.height):
                     image = self.tmx_data.get_tile_image(column, row, layer_count)
                     if image is not None:
-                        # if layer_count == 0:
-                        #     self.tile_coords_dict[(column, row)] = (x_coord, y_coord)
-                        x_coord = 16 * column 
-                        y_coord = 16 * row
+                        x_coord = cfg.TILE_SIZE * column 
+                        y_coord = cfg.TILE_SIZE * row
                         screen.blit(image, (x_coord, y_coord))
             layer_count = layer_count + 1
