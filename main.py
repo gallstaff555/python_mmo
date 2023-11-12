d485f96350d06df2aecc68c80b9a380ba737cbf3
David <davidsolo555@hotmail.com>
2023-11-12 11:24:39 -0800

    fix connection issue due to missing dependency

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 
-from game.game import Game
+import asyncio
+import socketio
+import time
+from game.game import start_game
 
-def main():
-    game = Game()
+sio = socketio.AsyncClient()
+
+@sio.event
+async def connect():
+    print("connection established")
+
+@sio.event
+async def disconnect():
+    print("disconnected from server")
+
+
+async def main():
+    print(f"Client starting...")
+    await sio.connect('http://localhost:5000')
+    await sio.wait()
+
+if __name__ == '__main__':
+    asyncio.run(main())
+    print("asdf")
+    time.sleep(5000)
+    game = start_game()
